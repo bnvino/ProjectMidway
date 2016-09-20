170aa7be4aeb692be7e26540958eb1378b2bbd07
bnvino <bnvino@gmail.com>
2016-09-21 00:39:49 +1000

    Check for duplicate Equipment ID entry and pop up an error message

--- EquipmentApp/MyScripts/AddEquipmentController.js
+++ EquipmentApp/MyScripts/AddEquipmentController.js
@@ -1,38 +1,24 @@
 ﻿app.controller('AddEquipmentController', function ($scope, SPACRUDService) {
-    $scope.Id = 0;
+    //$scope.Id = 0;
     //$scope.EquipmentName = "initial";
 
     $scope.save = function () {
         var Equipment = {
             Id: $scope.Id,
             EquipmentName: $scope.EquipmentName
-            //EquipmentName: $scope.EquipmentName
 
         };
 
         
         var promisePost = SPACRUDService.postEquipment(Equipment);
 
-        alert(JSON.stringify(promisePost.$$state.status));
 
-        var status = JSON.stringify(promisePost.$$state.status);
-
-        if (status == 1)
-        {
+        promisePost.then(function (pl) {
             alert("Equipment Saved Successfully.");
-        }
-
-        else
-        {
-            alert("Equipment ID already exist. Please enter a unique Id");
-        }
-
-        //promisePost.then(function (pl) {
-        //    alert("Equipment Saved Successfully.");
-        //},
-        //      function (errorPl) {
-        //          $scope.error = 'failure loading Equipment', errorPl;
-        //      });
+        },
+              function (errorPl) {
+                  alert("Equipment ID Already exist. Please enter a Unique ID");
+              });
 
     };
 
